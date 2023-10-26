ca7db174727e001ef642f228098f517a7f0dce60
flameuss <luis.henrique_campos@outlook.com.br>
2023-10-26 18:25:31 -0300

    update db.js

--- db.js
+++ db.js
@@ -1,7 +1,21 @@
 import 'dotenv/config';
 import postgres from 'postgres';
 
-const { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-export const sql = postgres(URL, {ssl: 'require' });
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+// const { HOST, PORT, DATABASE, USER, PASSWORD } = process.env;
+// const URL = `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+// export const sql = postgres(URL, { ssl: 'require' });
